c4b07bf68293b3fc18c81cbbe29a4cd744bfceb8
adelinechum <64668979+adelinechum@users.noreply.github.com>
2022-05-04 23:32:24 -0400

    commented out image

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ function openView (parameter) {
       // case "Gaslink": 
       //       var image = document.getElementById("GaslinkLink");
       //       $('iframe').append();
-      //       //image.style.display = 'flex';
+      //       image.style.display = 'flex';
                 
       //   break;
   
       case "Flatten": 
-            var image = document.getElementById("FlattenLink");
+            // var image = document.getElementById("FlattenLink");
             $('<iframe id= "FlattenLink" class="displayImages" src="https://centerforspatialresearch.github.io/summer2020_covidpolicies/"  allowtransparency="false" style="display: flex;" ></iframe>')
             .appendTo('body');
             //image.style.display = 'flex';
@@ -39,7 +39,7 @@ function openView (parameter) {
       break;
   
       case "Paradise":    
-            var image = document.getElementById("ParadiseLink");
+            // var image = document.getElementById("ParadiseLink");
             $('<iframe id= "ParadiseLink" class="displayImages" src="https://adelinechum.github.io/rebuildingParadiseInterface/"  allowtransparency="false" style="display: flex;" ></iframe>')
             .appendTo('body');
             //image.style.display = 'flex';          
@@ -47,7 +47,7 @@ function openView (parameter) {
       break;
   
       case "SafeSpace":
-        var image = document.getElementById("SafeSpaceLink");
+        // var image = document.getElementById("SafeSpaceLink");
         $('<iframe id= "SafeSpaceLink" class="displayImages" src="./SafeSpace.html"  allowtransparency="false" style="display: flex;" ></iframe>')
         .appendTo('body');
         //image.style.display = 'flex';    
@@ -55,7 +55,7 @@ function openView (parameter) {
       break;
   
       case "Housing":
-        var image = document.getElementById("HousingLink");
+        // var image = document.getElementById("HousingLink");
         $('<iframe id= "HousingLink" class="displayImages" src="https://adelinechum.github.io/36YearsOfHousing/"  allowtransparency="false" style="display: flex;" ></iframe>')
         .appendTo('body');
           
@@ -63,63 +63,63 @@ function openView (parameter) {
       break;
 
       case "Furniture":
-        var image = document.getElementById("FurnitureLink");
+        // var image = document.getElementById("FurnitureLink");
         $('<iframe id= "FurnitureLink" class="displayImages" src="./Furniture.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body'); 
 
       break;
   
       case "Care":
-        var image = document.getElementById("CareLink");
+        // var image = document.getElementById("CareLink");
         $('<iframe id= "CareLink" class="displayImages" src="./Care.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');
 
       break;
 
       case "Quilted":
-        var image = document.getElementById("QuiltedLink");
+        // var image = document.getElementById("QuiltedLink");
         $('<iframe id= "QuiltedLink" class="displayImages" src="./Quilt.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');    
 
       break;
 
       case "Pillow":
-        var image = document.getElementById("PillowLink");
+        // var image = document.getElementById("PillowLink");
         $('<iframe id= "PillowLink" class="displayImages" src="./Pillow.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');    
 
       break;
 
       case "SteroidPlants":
-        var image = document.getElementById("SteroidPlantsLink");
+        // var image = document.getElementById("SteroidPlantsLink");
         $('<iframe id= "SteroidPlantsLink" class="displayImages" src="./SteroidPlants.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');    
 
       break;
 
       case "MoMa":
-        var image = document.getElementById("MoMaLink");
+        // var image = document.getElementById("MoMaLink");
         $('<iframe id= "MoMaLink" class="displayImages" src="./Moma.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');    
 
       break;
 
       case "Refactoring":
-        var image = document.getElementById("RefactoringLink");
+        // var image = document.getElementById("RefactoringLink");
         $('<iframe id= "RefactoringLink" class="displayImages" src="./Refactoring.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');   
 
       break;
 
       case "Desert":
-        var image = document.getElementById("DesertLink");
+        // var image = document.getElementById("DesertLink");
         $('<iframe id= "DesertLink" class="displayImages" src="./Desert.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');   
 
       break;
 
       case "Wetlands":
-        var image = document.getElementById("WetlandsLink");
+        // var image = document.getElementById("WetlandsLink");
         $('<iframe id= "WetlandsLink" class="displayImages" src="./Wetlands.html"  allowtransparency="false" style="display: none;" ></iframe>')
         .appendTo('body');    
 
