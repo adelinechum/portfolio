3f432f36292d319c6a099333159e9c22df6bcaaf
adelinechum <64668979+adelinechum@users.noreply.github.com>
2025-12-07 10:02:09 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
 export default defineConfig({
-  base: '/r3f-portfolio/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
